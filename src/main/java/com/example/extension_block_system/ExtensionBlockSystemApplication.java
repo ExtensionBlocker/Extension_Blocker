7656282f6bf178cf0c96be74673a7d6b50ad91c8
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-04-16 16:17:47 +0900

    init project

---
+++ src/main/java/com/example/extension_block_system/ExtensionBlockSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.extension_block_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionBlockSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtensionBlockSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/extension_block_system/ExtensionBlockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extension_block_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionBlockSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
