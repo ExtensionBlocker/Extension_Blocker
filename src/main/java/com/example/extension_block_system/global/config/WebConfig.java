a79c5b30b1036e5644254fa859b436f8d639092c
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-04-19 17:57:33 +0900

    feat: web 배포로 인한 allowOrigins 주소 변경

--- src/main/java/com/example/extension_block_system/global/config/WebConfig.java
+++ src/main/java/com/example/extension_block_system/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://ec2-43-203-235-47.ap-northeast-2.compute.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
