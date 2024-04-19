bfc4a55ded2df7fac19bcd7ada60751a1eba9ed1
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-04-19 18:41:41 +0900

    feat: 로컬 테스트를 위한 allowedOrigin 수정

--- src/main/java/com/example/extension_block_system/global/config/WebConfig.java
+++ src/main/java/com/example/extension_block_system/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ec2-43-203-235-47.ap-northeast-2.compute.amazonaws.com")
+                .allowedOrigins("http://ec2-43-203-235-47.ap-northeast-2.compute.amazonaws.com", "http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
